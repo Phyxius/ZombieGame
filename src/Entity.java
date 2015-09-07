bd7449f3a391d0b1eb6bf2e0cb0a3b5269164dd9
Shea Polansky <shea@polansky.co>
2015-09-07 13:25:00 -0600

    Added Skeletons for Entity[Manager]

---
+++ src/Entity.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Shea on 2015-09-07.
+ * Class name and description go here.
+ */
+public interface Entity
+{
+  void draw(Graphics2D local, Graphics2D screen); //local = origin centered at upper-left corner of object, screen = origin at upper-left corner of screen
+  Point[] getPosition(); //returns array of grid squares occupied by object
+  boolean isSolid(); //solid objects cannot move into each other
+  void keyPressed(keyEventArgs e);
+  void update(EntityManager e); //called for each update tick, EntityManager contains methods to add/remove/etc entities
+  void onCollision(Entity other); //called when collided with other entity
+}
