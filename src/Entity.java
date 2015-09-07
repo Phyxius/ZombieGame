b9e18e7a278a412e4f4eef80f14fb5b07b9858bd
Shea Polansky <shea@polansky.co>
2015-09-07 13:43:39 -0600

    Stopped trying to write C# in Java

--- src/Entity.java
+++ src/Entity.java
@@ -9,7 +9,7 @@ public interface Entity
   void draw(Graphics2D local, Graphics2D screen); //local = origin centered at upper-left corner of object, screen = origin at upper-left corner of screen
   Point[] getPosition(); //returns array of grid squares occupied by object
   boolean isSolid(); //solid objects cannot move into each other
-  void keyPressed(keyEventArgs e);
+  void keyPressed(KeyEvent e);
   void update(UpdateManager e); //called for each update tick, EntityManager contains methods to add/remove/etc entities
   void onCollision(Entity other); //called when collided with other entity
 }
