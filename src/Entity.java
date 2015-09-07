61041335853cbb8d5e2c3f86794e10e9a76a062c
Shea Polansky <shea@polansky.co>
2015-09-07 14:25:13 -0600

    Switched Entity to abstract class

--- src/Entity.java
+++ src/Entity.java
@@ -7,13 +7,34 @@ import java.awt.geom.Rectangle2D;
  * Created by Shea on 2015-09-07.
  * Class name and description go here.
  */
-public interface Entity
+public abstract class Entity
 {
-  void draw(Graphics2D local, Graphics2D screen); //local = origin centered at upper-left corner of object, screen = origin at upper-left corner of screen
-  Point2D.Float getPosition(); //returns center point of object
-  Rectangle2D.Float getBoundingBox(); //returns bounding box of object
-  boolean isSolid(); //solid objects cannot move into each other
-  void keyPressed(KeyEvent e);
-  void update(UpdateManager e); //called for each update tick, EntityManager contains methods to add/remove/etc entities
-  void onCollision(Entity other); //called when collided with other entity
+  public void draw(Graphics2D local, Graphics2D screen) //local = origin centered at upper-left corner of object, screen = origin at upper-left corner of screen
+  {
+
+  }
+  public Point2D.Float getPosition() //returns center point of object
+  {
+    return null;
+  }
+  public Rectangle2D.Float getBoundingBox() //returns bounding box of object
+  {
+    return null;
+  }
+  public boolean isSolid() //solid objects cannot move into each other
+  {
+    return false;
+  }
+  public void keyPressed(KeyEvent e)
+  {
+
+  }
+  public void update(UpdateManager e) //called for each update tick, EntityManager contains methods to add/remove/etc entities
+  {
+
+  }
+  public void onCollision(Entity other) //called when collided with other entity
+  {
+
+  }
 }
