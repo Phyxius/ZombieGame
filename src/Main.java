de028718630ff4f3d1ecc92baa503c14fc47446e
Ari Rappaport <arappaport@unm.edu>
2015-09-07 14:42:29 -0600

    Added Main class and ZombieFrame class

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+/**
+ * Created by arirappaport on 9/7/15.
+ */
+public class Main
+{
+  public static void main(String[] args)
+  {
+    ZombieFrame zombieFrame = new ZombieFrame();
+    zombieFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    zombieFrame.setVisible(true);
+  }
+}
