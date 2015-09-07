1cdd6af5b342d7d69be0009cc34a8e5bc97d58e2
Shea Polansky <shea@polansky.co>
2015-09-07 17:12:36 -0600

    Convert SettingsManager to static values

---
+++ src/Settings.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+
+/**
+ * Created by Shea on 2015-09-07.
+ * Class name and description go here.
+ */
+public class Settings
+{
+  public static int frameRate = 60; //frames/sec
+  public static float zombieSpeed = 0.05f; //tiles/sec
+  public static float zombieDecisionRate = 2.0f; //sec
+  public static float zombieSmellRadius = 7f; //tiles
+  public static int tileSize = 80; //pixels
+}
