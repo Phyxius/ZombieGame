7a4c65fcc5e036b1bb32fd1c9066170822fa846c
Shea Polansky <shea@polansky.co>
2015-09-17 10:14:35 -0600

    Added test file

---
+++ src/TestClass.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Shea on 2015-09-17.
+ * Class name and description go here.
+ */
+public class TestClass
+{
+  public static void HelloWorld()
+  {
+    System.out.println("Hello, world!");
+  }
+}
